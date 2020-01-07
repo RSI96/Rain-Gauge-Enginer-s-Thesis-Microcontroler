5b940ea08464315c3155253cd9a2306b729904fd
radek.sikora <radek.sikora@bitcave.eu>
2020-01-07 19:10:44 +0100

    sending interval

--- arduino_software_for_rain_gauge_github.ino
+++ arduino_software_for_rain_gauge_github.ino
@@ -10,37 +10,49 @@ const long utcOffsetInSeconds = 3600;
 
 int hallSensor = 5;
 int prevState = HIGH;
-int id = -2;
+int id = 0;
+int prev_id = -1;
+unsigned long previousMillis = 0;
+const long interval = 10000;
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 void setup() {
-  
+ 
   Serial.begin(115200);
-  
+
   pinMode(hallSensor, INPUT);
   digitalWrite(hallSensor, HIGH);
   
   WiFi.begin("NAZWA_SIECI", "HASŁO_DO_SIECI");
-  
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println("Czekam na połączenie");
+    Serial.println("Waiting for connection");
   }
   timeClient.begin();
 }
-
+ 
 void loop() {
-  
+ 
   if (WiFi.status() == WL_CONNECTED) {
     
+    unsigned long currentMillis = millis();
+
     int currState = digitalRead(hallSensor);
     
     if(currState != prevState)
     {
+    prev_id = id;
     id++;
+    }
     
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (id != 0) {
+      
     timeClient.update();
     unsigned long epoch = timeClient.getEpochTime();
     String year = String((epoch/31556926)+1970);
@@ -51,30 +63,30 @@ void loop() {
     String date_time = date + " " + time_;
     
     StaticJsonDocument<300> JSONdocument;
-    
+
     JSONdocument["id"] = id;
-    JSONdocument["measurement"] = 0.115;
+    JSONdocument["measurement"] = id * 0.115;
     JSONdocument["date"] = date;
     JSONdocument["time"] = time_;
     JSONdocument["date_time"] = date_time;
-    
+
+    id = 0;
+ 
     char JSONmessageBuffer[300];
     serializeJsonPretty(JSONdocument, JSONmessageBuffer);
     Serial.println(JSONmessageBuffer);
-    
+ 
     HTTPClient http;
-    
+ 
     http.begin("http://ADRES_SERWERA:8080/all");
     http.addHeader("Content-Type", "application/json");
-    
+
     int httpCode = http.POST(JSONmessageBuffer);
-    String payload = http.getString();
-    
-    Serial.println(httpCode);
-    Serial.println(payload);
-    
+     
     http.end();
     }
+  }
+
     prevState = currState;
-  } else { Serial.println("Błąd w połączeniu WiFi");}
+  } else { Serial.println("Error in WiFi connection");}
 }
